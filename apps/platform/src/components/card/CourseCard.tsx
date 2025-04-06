5ba19d9e2ab49893d76a60ca3cb9f44a264297c9
Priyobroto Kar <priyobrotokar@gmail.com>
2025-04-06 19:09:26 +0530

    feat(platform): complete course sidebar

--- apps/platform/src/components/card/CourseCard.tsx
+++ apps/platform/src/components/card/CourseCard.tsx
@@ -4,16 +4,34 @@ import { Button } from '../ui/button';
 import { IconClock, IconShoppingCart, IconStack2 } from '@tabler/icons-react';
 import Image from 'next/image';
 import { formatPrice } from '@/lib/utils';
+import Link from 'next/link';
+
+export interface Lesson {
+  id: string;
+  title: string;
+  slug: string;
+  duration: string;
+}
+
+export interface Module {
+  id: string;
+  title: string;
+  slug: string;
+  lessons: Lesson[];
+}
 
 export interface Course {
+  id: string;
   title: string;
   subtitle: string;
+  slug: string;
   price: number;
   originalPrice: number;
   currency: string;
   duration: string;
   lessons: number;
   imageUrl: string;
+  modules: Module[];
 }
 
 interface CourseCardProps {
@@ -22,59 +40,63 @@ interface CourseCardProps {
 
 const CourseCard = ({ course }: CourseCardProps) => {
   return (
-    <Card>
-      <div>
-        <Image
-          src={course.imageUrl}
-          alt="Course Image"
-          width={277}
-          height={162}
-          className="w-full rounded-lg"
-        />
-      </div>
-
-      <Card.Content>
-        <Card.Header>
-          <Card.Subtitle>{course.subtitle}</Card.Subtitle>
-          <Card.Title>{course.title}</Card.Title>
-        </Card.Header>
-
-        <div className="space-y-2">
-          <div className="space-x-1">
-            <span className="text-xl">
-              {formatPrice({
-                amount: course.price,
-                currency: course.currency,
-              })}
-            </span>
-            <span className="text-muted-foreground text-sm line-through">
-              {formatPrice({
-                amount: course.originalPrice,
-                currency: course.currency,
-              })}
-            </span>
-          </div>
-          <div className="space-x-6">
-            <Card.Info>
-              <IconClock /> {course.duration}
-            </Card.Info>
-            <Card.Info>
-              <IconStack2 /> {course.lessons} Lessons
-            </Card.Info>
-          </div>
+    <Link
+      href={`/${course.slug}/${course.modules[0]?.slug}/${course.modules[0]?.lessons[0]?.slug}`}
+    >
+      <Card>
+        <div>
+          <Image
+            src={course.imageUrl}
+            alt="Course Image"
+            width={277}
+            height={162}
+            className="w-full rounded-lg"
+          />
         </div>
 
-        <Card.Footer>
-          <Button className="w-full">
-            <IconShoppingCart />
-            Buy Now
-          </Button>
-          <p className="text-muted-foreground text-center text-sm">
-            Earn certificate upon completion
-          </p>
-        </Card.Footer>
-      </Card.Content>
-    </Card>
+        <Card.Content>
+          <Card.Header>
+            <Card.Subtitle>{course.subtitle}</Card.Subtitle>
+            <Card.Title>{course.title}</Card.Title>
+          </Card.Header>
+
+          <div className="space-y-2">
+            <div className="space-x-1">
+              <span className="text-xl">
+                {formatPrice({
+                  amount: course.price,
+                  currency: course.currency,
+                })}
+              </span>
+              <span className="text-muted-foreground text-sm line-through">
+                {formatPrice({
+                  amount: course.originalPrice,
+                  currency: course.currency,
+                })}
+              </span>
+            </div>
+            <div className="space-x-6">
+              <Card.Info>
+                <IconClock /> {course.duration}
+              </Card.Info>
+              <Card.Info>
+                <IconStack2 /> {course.lessons} Lessons
+              </Card.Info>
+            </div>
+          </div>
+
+          <Card.Footer>
+            <Button className="w-full">
+              <IconShoppingCart />
+              Buy Now
+            </Button>
+            <p className="text-muted-foreground text-center text-sm">
+              Earn certificate upon completion
+            </p>
+          </Card.Footer>
+        </Card.Content>
+      </Card>
+    </Link>
   );
 };
 
--- apps/platform/src/components/sidebar/CourseSidebar.tsx
+++ apps/platform/src/components/sidebar/CourseSidebar.tsx
@@ -1,11 +1,68 @@
+'use client';
+import { demoCourse } from '@/lib/mocks';
 import Image from 'next/image';
 import React from 'react';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '../ui/accordion';
+import { IconCircleCaretRight } from '@tabler/icons-react';
+import { useParams } from 'next/navigation';
+import { cn } from '@/lib/utils';
 
 const CourseSidebar = () => {
+  const { details } = useParams<{
+    details: string[];
+  }>();
+  const lessonSlug = details[2];
+
   return (
-    <aside className="bg-card flex w-56 flex-col justify-between self-stretch border-r px-7 py-10">
+    <aside className="bg-card flex w-56 flex-col gap-4 self-stretch border-r px-7 py-10">
       <div className="space-y-20">
         <Image src={'/logo.svg'} alt="logo" width={71} height={21} />
+      </div>
+      <div className="space-y-14">
+        <div className="space-y-3">
+          <h1 className="text-xl">{demoCourse.title}</h1>
+          <p className="text-sm">{demoCourse.subtitle}</p>
+        </div>
+
+        <Accordion type="multiple" className="space-y-4">
+          {demoCourse.modules.map((module) => {
+            return (
+              <AccordionItem
+                className="data-[state=open]:bg-background data-[state=open]:border-border rounded-2xl border px-4 data-[state=closed]:border-transparent"
+                key={module.id}
+                value={module.id}
+              >
+                <AccordionTrigger className="text-md [&>svg]:hidden">
+                  {module.title}
+                </AccordionTrigger>
+                <AccordionContent className="space-y-5">
+                  {module.lessons.map((lesson) => {
+                    return (
+                      <div
+                        key={lesson.id}
+                        className={cn(
+                          'flex gap-2',
+                          lesson.slug === lessonSlug && 'text-primary',
+                        )}
+                      >
+                        <IconCircleCaretRight className="size-6" />
+                        <div className="space-y-2">
+                          <p className="text-sm-md">{lesson.title}</p>
+                          <span>{lesson.duration}</span>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </AccordionContent>
+              </AccordionItem>
+            );
+          })}
+        </Accordion>
       </div>
     </aside>
   );
