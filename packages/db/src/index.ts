c9c5c27ecac28115a5860fa22b9fb57bbcf29a3b
Priyobroto Kar <priyobrotokar@gmail.com>
2025-08-10 19:38:37 +0530

    fix(www): fix bulid

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,11 +1,11 @@
-import { PrismaClient } from '../prisma/generated/client';
+import { PrismaClient } from '../generated/prisma';
 
 const globalForPrisma = globalThis as typeof globalThis & {
   prisma?: PrismaClient;
 };
 
 export const db = globalForPrisma.prisma || new PrismaClient();
-export * from '../prisma/generated/client';
+export * from '../generated/prisma';
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = db;
